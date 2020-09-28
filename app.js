aeb21879063f119a90f19d48d0e0840ff84cb48b
BoitshokoM <BoitshokoM@blts.co.za>
2020-09-28 13:58:08 +0200

    public

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser')
 
@@ -7,13 +9,14 @@ const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 
 app.use(shopRoutes)
 
-app.use((req, res) => {
-    res.status(404).send('<h1>Page not found</h1>')
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', 'page-not-found.html'));
 })
 
 app.listen(3000);
