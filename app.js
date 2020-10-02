3023e2cf01270f77e01c2bddfa39a2023e62d576
BoitshokoM <BoitshokoM@blts.co.za>
2020-10-02 09:37:23 +0200

    pug

--- app.js
+++ app.js
@@ -5,15 +5,19 @@ const bodyParser = require('body-parser')
 
 const app = express();
 
-const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
+app.set('view engine', 'pug');
+app.set('views', 'views')
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 
-app.use(shopRoutes)
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', 'page-not-found.html'));
